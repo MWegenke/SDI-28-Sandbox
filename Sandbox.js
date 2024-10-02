fa3ae5c96c810636863f51ebe92d05065cbc432f
CatCode23 <andrewcatalano23@gmail.com>
2024-10-02 14:25:03 -0400

    trying to learn more about built in functions with strings

--- Sandbox.js
+++ Sandbox.js
@@ -3,31 +3,39 @@
 
 // age = 51;
 
-let age = 51;
+// let age = 51;
 
-const myName = "Wagonkey";
+// const myName = "Wagonkey";
 
-function mattsFunction(currAge, name){
-  //The 51 year old Wagonkey is the best!
-  console.log(`The ${currAge} year old ${name} is the best!`)
-  // console.log("The ", age, " year old ", myName, " is the best!")
-}
+// function mattsFunction(currAge, name){
+//   //The 51 year old Wagonkey is the best!
+//   console.log(`The ${currAge} year old ${name} is the best!`)
+//   // console.log("The ", age, " year old ", myName, " is the best!")
+// }
 
-mattsFunction(age, myName)
+// mattsFunction(age, myName)
 
-function happyBirthday(){
-  //in memory somewhere 51,51,51,51,etc
-  age++
-}
+// function happyBirthday(){
+//   //in memory somewhere 51,51,51,51,etc
+//   age++
+// }
 
-happyBirthday()
-happyBirthday()
-happyBirthday()
-happyBirthday()
-happyBirthday()
+// happyBirthday() 
 
-console.log(age)
+
+// console.log(age)
 
 // mattsFunction(happyBirthday(age) ,myName)
 
-// happyBirthday() 
+let Name = "andrew";
+
+function reverso(str){
+  //parses the string out creating an array
+  var stringSplit = str.split("")
+  //reverses the array
+  var arrayReverse = stringSplit.reverse()
+  var arrayJoin = arrayReverse.join("")
+  return arrayJoin
+}
+
+console.log(reverso(Name));
