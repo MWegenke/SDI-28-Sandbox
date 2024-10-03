515b4805bd2e27884cdf3e23de0ff14c5a26a472
dimmestsalmon <appelnephi@gmail.com>
2024-10-03 11:17:02 -0400

    commented out existing code so I can use VS code for classroom

--- Sandbox.js
+++ Sandbox.js
@@ -3,55 +3,55 @@
 
 // age = 51;
 
-let age = 51;
+// let age = 51;
 
-const myName = "Wagonkey";
+// const myName = "Wagonkey";
 
-function mattsFunction(currAge, name){
+// function mattsFunction(currAge, name){
   //The 51 year old Wagonkey is the best!
-  console.log(`The ${currAge} year old ${name} is the best!`)
+  // console.log(`The ${currAge} year old ${name} is the best!`)
   // console.log("The ", age, " year old ", myName, " is the best!")
-}
+// }
 
-mattsFunction(age, myName)
+// mattsFunction(age, myName)
 
-function happyBirthday(){
+// function happyBirthday(){
   //in memory somewhere 51,51,51,51,etc
-  age++
-}
+//   age++
+// }
 
-happyBirthday()
-happyBirthday()
-happyBirthday()
-happyBirthday()
-happyBirthday()
+// happyBirthday()
+// happyBirthday()
+// happyBirthday()
+// happyBirthday()
+// happyBirthday()
 
-console.log(age)
+// console.log(age)
 
 // mattsFunction(happyBirthday(age) ,myName)
 
 // happyBirthday()
 
 // Rock paper scissors game
-const rps = (p1, p2) => {
-  var who = true;
-  var wins;
-  if(p1 === p2){
-    wins = "Draw!"
-  } else {
-    if(p1 === "scissors" && p2 === "rock"){
-      who = false;
-    } else if(p1 === "paper" && p2 === "scissors"){
-      who = false;
-    } else if(p1 === "rock" && p2 === "paper"){
-      who = false;
-    }
-    if (who){
-      wins = "Player 1 won!"
-    }
-    else if(!who){
-      wins = "Player 2 won!"
-    }
-  }
-  return wins;
-};
+// const rps = (p1, p2) => {
+//   var who = true;
+//   var wins;
+//   if(p1 === p2){
+//     wins = "Draw!"
+//   } else {
+//     if(p1 === "scissors" && p2 === "rock"){
+//       who = false;
+//     } else if(p1 === "paper" && p2 === "scissors"){
+//       who = false;
+//     } else if(p1 === "rock" && p2 === "paper"){
+//       who = false;
+//     }
+//     if (who){
+//       wins = "Player 1 won!"
+//     }
+//     else if(!who){
+//       wins = "Player 2 won!"
+//     }
+//   }
+//   return wins;
+// };
