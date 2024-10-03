1d93451c3531818fcec0b52b92ef06bc4c2e2980
Christopher Kosik <37384678+skorm11x@users.noreply.github.com>
2024-10-03 09:10:06 -0600

    fixed capitalization issues for sandbox files

--- sandbox.js
+++ sandbox.js
@@ -1,4 +1,4 @@
-
+//JavaScript Fundamentals I
 // var age;
 
 // age = 51;
@@ -8,17 +8,17 @@
 // const myName = "Wagonkey";
 
 
-function mattsFunction(currAge, name){
-  //The 51 year old Wagonkey is the best!
-  console.log(`The ${currAge} year old ${name} is the best!`)
-  // console.log("The ", age, " year old ", myName, " is the best!")
-}
+// function mattsFunction(currAge, name){
+//   //The 51 year old Wagonkey is the best!
+//   console.log(`The ${currAge} year old ${name} is the best!`)
+//   // console.log("The ", age, " year old ", myName, " is the best!")
+// }
 
-mattsFunction(age, myName)
-function happyBirthday(){
-  //in memory somewhere 51,51,51,51,etc
-  age++
-}
+// mattsFunction(age, myName)
+// function happyBirthday(){
+//   //in memory somewhere 51,51,51,51,etc
+//   age++
+// }
 
 // happyBirthday() 
 
@@ -31,15 +31,49 @@ function happyBirthday(){
 
 // let myParagraphElement = document.querySelector('p');
 // console.log("Here we are:", myParagraphElement);
-let Name = "andrew";
+// let Name = "andrew";
 
-function reverso(str){
-  //parses the string out creating an array
-  var stringSplit = str.split("")
-  //reverses the array
-  var arrayReverse = stringSplit.reverse()
-  var arrayJoin = arrayReverse.join("")
-  return arrayJoin
+// function reverso(str){
+//   //parses the string out creating an array
+//   var stringSplit = str.split("")
+//   //reverses the array
+//   var arrayReverse = stringSplit.reverse()
+//   var arrayJoin = arrayReverse.join("")
+//   return arrayJoin
+// }
+
+// console.log(reverso(Name));
+
+
+//JavaScript Fundamentals II
+
+// let temp = 10
+// console.log(temp >= 10)
+// console.log(temp > 10 || temp == 10)
+//is temp < 20 and is temp a number
+// console.log((temp < 20 && typeof temp))
+// console.log((typeof temp && temp < 20))
+
+// console.log(true)
+
+// console.log(!true)
+
+
+// if (!true){
+//   console.log(temp)
+// }
+
+// let x = 11
+// let y = 'fun'
+
+// if("NaN"){
+//   console.log("Thats true")
+// }else{
+//   console.log("That b false bruv")
+// }
+
+//Loop through what
+
+for(let i = 1; i <= 10; i++ ){
+  console.log(`Current count : ${i}`)
 }
-
-console.log(reverso(Name));
