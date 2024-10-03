7a1c39ecec8f3b5f474620e39bf5dbc60f7e0d42
CatCode23 <andrewcatalano23@gmail.com>
2024-10-03 10:57:16 -0400

    delete old sandbox.js and .html

--- sandbox.js
+++ sandbox.js
@@ -8,38 +8,43 @@
 // const myName = "Wagonkey";
 
 
-function mattsFunction(currAge, name){
-  //The 51 year old Wagonkey is the best!
-  console.log(`The ${currAge} year old ${name} is the best!`)
-  // console.log("The ", age, " year old ", myName, " is the best!")
+// function mattsFunction(currAge, name){
+//   //The 51 year old Wagonkey is the best!
+//   console.log(`The ${currAge} year old ${name} is the best!`)
+//   // console.log("The ", age, " year old ", myName, " is the best!")
+// }
+
+// mattsFunction(age, myName)
+// function happyBirthday(){
+//   //in memory somewhere 51,51,51,51,etc
+//   age++
+// }
+
+// // happyBirthday() 
+
+
+// // console.log(age)
+
+// // mattsFunction(happyBirthday(age) ,myName)
+
+// // happyBirthday() 
+
+// // let myParagraphElement = document.querySelector('p');
+// // console.log("Here we are:", myParagraphElement);
+// let Name = "andrew";
+
+// function reverso(str){
+//   //parses the string out creating an array
+//   var stringSplit = str.split("")
+//   //reverses the array
+//   var arrayReverse = stringSplit.reverse()
+//   var arrayJoin = arrayReverse.join("")
+//   return arrayJoin
+// }
+
+// console.log(reverso(Name));
+
+for(let i = 0; i <= 10; i+= .5){
+  //let x = i*.5
+  console.log(`number: ${x}`)
 }
-
-mattsFunction(age, myName)
-function happyBirthday(){
-  //in memory somewhere 51,51,51,51,etc
-  age++
-}
-
-// happyBirthday() 
-
-
-// console.log(age)
-
-// mattsFunction(happyBirthday(age) ,myName)
-
-// happyBirthday() 
-
-// let myParagraphElement = document.querySelector('p');
-// console.log("Here we are:", myParagraphElement);
-let Name = "andrew";
-
-function reverso(str){
-  //parses the string out creating an array
-  var stringSplit = str.split("")
-  //reverses the array
-  var arrayReverse = stringSplit.reverse()
-  var arrayJoin = arrayReverse.join("")
-  return arrayJoin
-}
-
-console.log(reverso(Name));
