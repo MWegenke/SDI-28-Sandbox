27e81b4de4c729dcc4cf9cf172395ec54a9c7938
dimmestsalmon <appelnephi@gmail.com>
2024-11-19 10:24:45 -0500

    added remainign docker commands

--- Sandbox.js
+++ Sandbox.js
@@ -239,5 +239,12 @@
 
 // export default App;
 
-
+// START POSTGRES ON DOCKER
+// docker pull postgres
+// mkdir -p $HOME/docker/volumes/postgres *******IF NEEDED
+// docker run --rm --name pg-docker -e POSTGRES_PASSWORD=docker -d -p 5432:5432 -v $HOME/docker/volumes/postgres:/var/lib/postgresql/data postgres
+// docker ps -a ****LIST DOCKER IMAGES
+// docker exec -it <PSQL-Container-ID> bash
+// psql -U postgres
+// \c <database name>
 
